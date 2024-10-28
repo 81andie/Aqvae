33ec6e48c992a60e5e684b5428cb4e212f64fc02
81andie <133395577+81andie@users.noreply.github.com>
2024-10-28 17:44:27 +0100

    pasando los datos al componente sidenav

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -12,34 +12,31 @@ import { Features } from '../../interfaces/features.interface';
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.css'
 })
-export class SidenavComponent  implements OnInit {
+export class SidenavComponent implements OnInit {
 
-  maxVolum: number = 0;  // Inicialización con un valor por defecto
-  maxNivell: number = 0;
+
 
 
   public measurements: any[] = [];
 
-  constructor(private PlacesService: PlacesService){}
+  constructor(private PlacesService: PlacesService) { }
 
   ngOnInit(): void {
     this.PlacesService.measurements$.subscribe(measurements => {
-      this.measurements = measurements; // Actualizar las mediciones
+
+      this.measurements = measurements;
+      if (this.measurements.length > 0) {
+        this.isSidebarVisible = true
+
+      }
+      // Actualizar las mediciones
       console.log('Mediciones en Sidenav:', this.measurements);
-      this.calculateMaxValues();
+
     });
 
-    }
+  }
 
-    private calculateMaxValues(): void {
-      if (this.measurements.length > 0) {
-        this.maxVolum = Math.max(...this.measurements.map(m => m.volum_embassat));
-        this.maxNivell = Math.max(...this.measurements.map(m => m.nivell_absolut));
-      } else {
-        this.maxVolum = 0;
-        this.maxNivell = 0;
-      }
-    }
+
 
   public isSidebarVisible: boolean = false;
 
@@ -48,10 +45,28 @@ export class SidenavComponent  implements OnInit {
   }
 
 
+  getWaveHeight(nivellAbsolut: number): string {
+
+    if (nivellAbsolut >= 100) {
+     return '90%';
+    } else if (nivellAbsolut >= 75) {
+      return '70%';
+    } else if (nivellAbsolut >= 50) {
+      return '50%';
+    } else if (nivellAbsolut >= 25) {
+      return '30%';
+    }else{
+      return '10%'
+    }
 
 
 
-}
+    }
+
+
+
+
+  }
 
 
 
